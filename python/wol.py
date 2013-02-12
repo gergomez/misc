3050d401acc60192708b4fba76cce382150914b9
German Gomez <gergomez@gmail.com>
2013-02-12 15:31:55 +0100

    WakeOnLan python scrypt

---
+++ python/wol.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import sys
+import socket
+
+if len(sys.argv) < 2:
+    print sys.argv[0] + ' <00:11:22:33:44:55>'
+    sys.exit(1)
+
+hw_addr_byte = sys.argv[1].split(':')
+if len(hw_addr_byte) != 6:
+    print 'Invalid ethernet addr: "' + sys.argv[1] + '"'
+    sys.exit(1)
+
+hw_addr = reduce(lambda a, b: (a + chr(int(b,16))), hw_addr_byte, '')
+msg = '\xff'*6 + hw_addr*16
+print 'Sending WakeOnLan to ' + sys.argv[1] + '...'
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.sendto(msg, ('<broadcast>', 7))
+s.close()
