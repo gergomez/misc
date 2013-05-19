a6c2a57eb60bf6e699f48225152b8118583fe71a
German Gomez <gergomez@gmail.com>
2013-05-19 20:57:30 +0200

    Funny pointers!

---
+++ algo/funny_pointers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int *f1(int n) {
+    static int i[3] = {1,2,3};
+    return &i[n];
+}
+
+int *f2(int n) {
+    static int i[3] = {11,12,13};
+    return &i[n];
+}
+
+int *(*f(int n))(int) {
+    int *(*a[])(int) = {f1, f2};
+    return a[n];
+}
+
+int main() {
+    int *(*fp)(int) = f(1);
+    printf("%d\n", *fp(2));
+    return 0;
+}
