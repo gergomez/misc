f302b8725aa6e65d6c72a40e38c488ab16c89ff1
German Gomez <gergomez@gmail.com>
2026-02-23 10:38:34 +0100

    Added .gitignore and unordered_map from tr1:: to std::

--- algo/count.cpp
+++ algo/count.cpp
@@ -1,4 +1,4 @@
-#include <tr1/unordered_map>
+#include <unordered_map>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -6,14 +6,14 @@ using namespace std;
 #define MAXLINE 4096
 
 int main() {
-    tr1::unordered_map <string, int> m;
+    std::unordered_map <string, int> m;
     char buf[MAXLINE];
 
     while(! cin.eof()) {
         cin.getline(buf, MAXLINE);
         ++m[buf];
     }
-    for(tr1::unordered_map<string, int>::iterator it = m.begin(); it != m.end(); ++it) {
+    for(std::unordered_map<string, int>::iterator it = m.begin(); it != m.end(); ++it) {
         cout << it->first << "\t" << it->second << endl;
     }
 }
